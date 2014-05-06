38620243745b07564eba5e486711cf57913f072e
rljohnsn <johnsonrl@yahoo.com>
2014-05-06 13:06:04 -0700

    adding packer definition for chef server from scratch, and the docker script

---
+++ scripts/chef-server.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd /usr/local/src
+
+sudo echo 'APT::Default-Release "wheezy";' >> /etc/apt/apt.conf
+sudo echo 'deb http://ftp.de.debian.org/debian/ jessie main' >> /etc/apt/sources.list.d/debian-jessie.list
+sudo echo 'deb-src http://ftp.de.debian.org/debian/ jessie main' >> /etc/apt/sources.list.d/debian-jessie.list
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+sudo DEBIAN_FRONTEND=noninteractive apt-get -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install libc6/jessie
+
+sudo curl -C - -L -O  https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.12-1.ubuntu.12.04_amd64.deb
+sudo dpkg -i chef-server_11.0.12-1.ubuntu.12.04_amd64.deb
+sudo chef-server-ctl reconfigure
+#echo "Next run chef-server-ctl reconfigure"
---
+++ scripts/docker.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -sL https://get.docker.io/ | sh
