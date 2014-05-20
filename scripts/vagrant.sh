d8d03ad9b57428e63b2ba77d35502f021d499792
rljohnsn <johnsonrl@yahoo.com>
2014-05-20 11:14:56 -0700

    using curl short meter format

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -7,7 +7,7 @@ useradd -G sudo -p $(perl -e'print crypt("vagrant", "vagrant")') -m -s /bin/bash
 
 # Install vagrant keys
 mkdir -pm 700 /home/vagrant/.ssh
-curl -Lo /home/vagrant/.ssh/authorized_keys \
+curl -# -Lo /home/vagrant/.ssh/authorized_keys \
   'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
